b0e2dcb77164edc844daf10af61d0527dd8eca9a
hawang <654296217@qq.com>
2015-03-26 18:32:38 +0800

    add lessCompiler and serve task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,27 @@
 var gulp = require('gulp');
 
+var less = require('gulp-less');
+
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('lessCompiler', function() {
+
+  gulp.src('public/style/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('.tmp/style'))
+
+});
+
+gulp.task('serve', ['lessCompiler'], function() {
+
+  gulp.watch([
+    'public/style/*.less'
+  ]).on('change', reload);
+
+  gulp.watch('public/style/*.less', ['lessCompiler', reload]);
+
+});
+
 gulp.task('default');
