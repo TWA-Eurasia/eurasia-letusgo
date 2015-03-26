a53c2c69a3da96c4d60a4174e81432305f3a615f
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-27 01:08:59 +0800

    home view

--- model/mainCategory.js
+++ model/mainCategory.js
@@ -1,3 +1,14 @@
-/**
- * Created by zhzhang on 3/27/15.
- */
+'use strict';
+var _ = require('lodash');
+var mongoose = require('mongoose');
+
+var mainCategorySchema = new mongoose.Schema({
+  name: String,
+  childId: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ChildCategory'
+  }]
+});
+var mainCategory = mongoose.model('MainCategory', mainCategorySchema);
+
+module.exports = mainCategory;
--- router/route/home.js
+++ router/route/home.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var MainCategory = require('../../model/mainCategory.js');
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
 
-  res.render('home');
+  MainCategory.find(function (err, data) {
 
+    if (err) return next(err);
+    data = [
+
+      {name: '男装/户外运动'},
+      {name: '女鞋/男鞋/箱包'},
+      {name: '女装/内衣'},
+      {name: '化妆品/个人护理'},
+      {name: '手机/数码/电脑办公'},
+      {name: '母婴玩具'},
+      {name: '零食/进口食品/酒'},
+      {name: '大家电/生活电器'},
+      {name: '家居建材'},
+      {name: '汽车/配件/用品'}
+    ];
+    res.render('home', {mainCategories: data});
+  });
 });
 
 module.exports = router;
