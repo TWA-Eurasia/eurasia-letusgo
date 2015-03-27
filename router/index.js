963ac7391739d6cf1592382301be9bf010ea7e89
jzy <ZiyiKing@outlook.com>
2015-03-27 18:35:16 +0800

    creat cart.jade and cart.js to display in cart items

--- router/index.js
+++ router/index.js
@@ -3,4 +3,5 @@ module.exports = function (app) {
     app.use('/helloWorld', require('./route/helloWorld'));
 
     app.use('/api/item', require('./route/item'));
+    app.use('/:user/cart',require('./route/cart'));
 };
