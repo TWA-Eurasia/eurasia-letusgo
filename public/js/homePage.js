11c15de49debd5885632fe62cf4feed318656afb
JacobKangJie <jiekang@thoughtworks.com>
2015-03-27 20:31:16 +0800

    add homePage.js

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,3 +1,25 @@
-/**
- * Created by jiekang on 3/27/15.
- */
+'use strict';
+require.config({
+  baseUrl: './',
+  paths: {
+    'jquery': './jquery/dist/jquery',
+    'semantic': './semantic-ui/dist/semantic',
+  }
+});
+
+require(['semantic', 'jquery'], function (semantic, $) {
+
+  $(document).ready(function () {
+
+    console.log($('.top')[0].addClass('dropdown'));
+
+    $('.top')[0].append(
+      '<div class="menu">'+
+      '<a class="item">Edit Profile</a>'+
+      '<a class="item"><i class="globe icon"></i> Choose Language</a>'+
+      '<a class="item"><i class="settings icon"></i> Account Settings</a>'+
+      '</div> '
+    );
+  });
+
+});
