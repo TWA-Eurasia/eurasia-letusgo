9b619e99e56e5158c7b3886931117279d0a7f74e
JacobKangJie <jiekang@thoughtworks.com>
2015-03-27 20:30:47 +0800

    complete home get router

--- router/route/home.js
+++ router/route/home.js
@@ -2,15 +2,23 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Category = require('../../model/category.js');
+var Subcategory = require('../../model/subcategory.js');
 
 
 router.get('/', function (req, res, next) {
 
+
+  //Category.findById('551531b73f0d54290638592b', function(err, categories){
+  //
+  //  console.log(categories);
+  //  categories.subCategories[0] = '551536534afbae0a0758a009';
+  //  categories.save();
+  //  console.log(categories);
+  //});
+
   Category.find(function (err, categories) {
-    console.log(categories);
     res.render('home',{categories: categories})
   });
-
 
 });
 
