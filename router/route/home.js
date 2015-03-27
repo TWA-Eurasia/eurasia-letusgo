22ce9a81e2f57b22c7d1dee6f26ad287520adeb5
JacobKangJie <jiekang@thoughtworks.com>
2015-03-27 18:34:31 +0800

    category achieved

--- router/route/home.js
+++ router/route/home.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Category = require('../../model/category.js');
 
-router.get('/', function (req, res) {
-  res.render('home');
+
+router.get('/', function (req, res, next) {
+
+  Category.find(function (err, categories) {
+    console.log(categories);
+    res.render('home',{categories: categories})
+  });
+
 
 });
 
