b7036919318fe04e605f63d3ea68d19df38455d4
JacobKangJie <jiekang@thoughtworks.com>
2015-03-27 11:57:25 +0800

    config the home router

--- router/index.js
+++ router/index.js
@@ -1,6 +1,5 @@
 module.exports = function (app) {
 
-    app.use('/helloWorld', require('./route/helloWorld'));
+  app.use('/', require('./route/home'));
 
-    app.use('/api/item', require('./route/item'));
 };
