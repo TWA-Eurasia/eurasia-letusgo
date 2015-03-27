3446dddb2155f5955f0c941e56b367c287a1d59f
hawang <654296217@qq.com>
2015-03-27 12:05:10 +0800

    generate config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+System.config({
+  "baseURL": "/",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "lodash": "github:lodash/lodash@3.6.0",
+    "semantic-ui": "github:Semantic-Org/Semantic-UI@1.11.5",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
+  }
+});
+
