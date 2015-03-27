7a750a393b82be69a1662547f2a2d5ac36e17ea2
hawang <654296217@qq.com>
2015-03-27 18:28:37 +0800

    load jquery from jspm

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ System.config({
 
 System.config({
   "map": {
-    "jquery": "github:jquery/jquery@2.1.3",
+    "jquery": "github:components/jquery@2.1.3",
     "lodash": "github:lodash/lodash@3.6.0",
     "semantic-ui": "github:Semantic-Org/Semantic-UI@1.11.5",
     "traceur": "github:jmcriffey/bower-traceur@0.0.87",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
-    "sizzle": "github:jquery/jquery@2.1.3/sizzle/dist/sizzle"
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
   }
 });
+
