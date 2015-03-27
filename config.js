01c928379763f1e5acd8f5af3ac71ef99a2e67fb
hawang <654296217@qq.com>
2015-03-27 13:55:28 +0800

    add sizzle mapping

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ System.config({
     "lodash": "github:lodash/lodash@3.6.0",
     "semantic-ui": "github:Semantic-Org/Semantic-UI@1.11.5",
     "traceur": "github:jmcriffey/bower-traceur@0.0.87",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
+    "sizzle": "github:jquery/jquery@2.1.3/sizzle/dist/sizzle"
   }
 });
-
