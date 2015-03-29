a906a34745b50cf673afe9dbf3de9062dc5ce03b
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-29 15:21:03 +0800

    complete second item

--- router/route/home.js
+++ router/route/home.js
@@ -6,28 +6,14 @@ var Subcategory = require('../../model/subCategory.js');
 
 router.get('/', function (req, res, next) {
 
-
-  //Category.findById('551531b73f0d54290638592b', function(err, categories){
-  //
-  //  console.log(categories);
-  //  categories.subCategories[0] = '551536534afbae0a0758a009';
-  //  categories.save();
-  //  console.log(categories);
-  //});
-
-  Category.findById('5517724c40bcab200a6c53a5', function(err, data) {
-    console.log(data.subCategories[0].name);
-  });
-  Category.find(function (err, categories) {
-    console.log(categories);
-    res.render('home',{categories: categories})
-  });
-  //
-  //Category.find()
-  //  .populate('subCategories', 'name superCategory', null)
-  //  .exec(function(err, cartItems) {
-  //    res.render('cart',{cartItems:cartItems});
-  //  });
+  //var ids = ['5517a250964677899617dcba','5517a25a964677899617dcbb'];
+  //Category.createCategory('家居建材', ids);
+  Category.find()
+    .populate('subCategories', 'name superCategory', null)
+    .exec(function(err, categories) {
+      //console.log(categories);
+      res.render('home',{categories: categories});
+    });
 
 });
 
