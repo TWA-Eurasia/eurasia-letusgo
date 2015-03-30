86d3ad2155b1a97a36b9c7b5a8dd9fa1faac3d64
liurong <570899307@qq.com>
2015-03-30 14:04:57 -0400

    finish the function of add and verify numberInput

--- public/js/itemDetails.js
+++ public/js/itemDetails.js
@@ -2,8 +2,31 @@ var $ = require('jquery');
 
 $(document).ready(function () {
 
+
   $('i.minus').on('click',function () {
-    
-    $('#numberInput').val($('#numberInput').val() -1);
+
+    var numberInput = parseInt($('#numberInput').val());
+
+    if (numberInput !== 1) {
+      $('#numberInput').val(numberInput - 1);
+    }
   });
+
+  $('i.add').on('click',function () {
+
+    var numberInput = parseInt($('#numberInput').val());
+
+    $('#numberInput').val(numberInput + 1);
+  });
+
+  $('#numberInput').on('mouseout', function () {
+
+    var numberInput = parseInt($('#numberInput').val());
+
+    if(isNaN(numberInput)) {
+      $('#numberInput').val(1);
+    }
+  });
+
+
 });
