bbc825290ad52882b8aeab92775e7a4fe2d54102
liurong <570899307@qq.com>
2015-03-30 09:58:42 -0400

    add app.use_itemDetails

--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
 
     app.use('/helloWorld', require('./route/helloWorld'));
+    app.use('/itemDetails', require('./route/itemDetails'));
 
     app.use('/api/item', require('./route/item'));
 };
