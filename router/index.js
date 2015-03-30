4798704291e4cbbd2bd85e029e160cb7ff139cb4
liurong <570899307@qq.com>
2015-03-30 09:58:42 -0400

    add app.use_itemDetails

--- router/index.js
+++ router/index.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
 
     app.use('/', require('./route/home'));
     app.use('/helloWorld', require('./route/helloWorld'));
+    app.use('/itemDetails', require('./route/itemDetails'));
 
     app.use('/api/item', require('./route/item'));
 
