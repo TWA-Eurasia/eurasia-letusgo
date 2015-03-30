8ee053c9dd62d8f7c597dbe2f35ed7d97f54db40
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-30 18:32:31 +0800

    delete useless js code

--- router/route/home.js
+++ router/route/home.js
@@ -4,15 +4,11 @@ var router = express.Router();
 var Category = require('../../model/category.js');
 var Subcategory = require('../../model/subcategory.js');
 
+router.get('/', function (req, res) {
 
-router.get('/', function (req, res, next) {
-
-  //var ids = ['55190d34964677899617dcbf','55190d44964677899617dcc0','55190d68964677899617dcc1','55190d94964677899617dcc2'];
-  //Category.createCategory('零食/进口食品/酒', ids);
   Category.find()
     .populate('subCategories', 'name', null)
     .exec(function(err, categories) {
-      //console.log(categories);
       res.render('home',{categories: categories});
     });
 
