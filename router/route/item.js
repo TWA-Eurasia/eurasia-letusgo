6c635a2dfe882495a28a0ff3ac5eb9abe10e0630
fgcui <fgcui1204@163.com>
2015-03-30 16:21:02 +0800

    modified item.js

--- router/route/item.js
+++ router/route/item.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var Item = require('../../model/item');
 
 router.get('/', function (req, res) {
-    Item.create({
-      name: '哇哈哈',
-      unit: '瓶',
-      brand: '娃哈哈',
-      leftNumber: 100,
-      specification: '350ml',
-      price: 8
-    }, function (err, item) {
-      res.send('items');
-    });
+    //Item.create({
+    //  name: '哇哈哈',
+    //  unit: '瓶',
+    //  brand: '娃哈哈',
+    //  leftNumber: 100,
+    //  specification: '350ml',
+    //  price: 8
+    //}, function (err, item) {
+    //  res.send('items');
+    //});
 });
 
 module.exports = router;
