5d0947b64a4f7912cb34d7c513d0e4c270460ddc
fgcui <fgcui1204@163.com>
2015-03-31 13:40:20 +0800

    modify modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -6,18 +6,34 @@ require('semantic-ui');
 $(document).ready(function () {
 
   $('#pay').on('click', function () {
-    $('.small.modal')
-      .modal('show');
-
+    //$('.small.modal')
+    //  .modal('show');
+    //$('.small.modal')
+    //  .modal('attach events', '.first.modal .button')
+    //;
+    $('.coupled.modal')
+      .modal({
+        allowMultiple: true
+      })
+    ;
+    //open second modal on button click
+    $('.second.modal')
+      .modal('attach events', '.first.modal .button')
+    ;
+// show first immediately
+    $('.first.modal')
+      .modal('show')
+    ;
   });
 });
 
+//// initialize all modals
 
-$(document).ready(function () {
-
-  $('#ispay').on('click', function () {
-
-
-
-  });
-});
+//$(document).ready(function () {
+//
+//  $('#ispay').on('click', function () {
+//
+//
+//
+//  });
+//});
