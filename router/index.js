7b905cd254135235f2741913c770d1aa0b13b87d
yfwang <yufei930101@gmail.com>
2015-03-31 10:01:53 +0800

    add router of index

--- router/index.js
+++ router/index.js
@@ -3,6 +3,7 @@ module.exports = function (app) {
     app.use('/', require('./route/home'));
     app.use('/helloWorld', require('./route/helloWorld'));
     app.use('/itemDetails', require('./route/itemDetails'));
+    app.use('/index', require('./route/index'));
 
     app.use('/api/item', require('./route/item'));
 
