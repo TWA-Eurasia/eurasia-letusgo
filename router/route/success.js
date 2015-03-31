fc4cdf4a5c1c23fce6a6893847a73cfe7ca59c54
fgcui <fgcui1204@163.com>
2015-03-31 13:39:45 +0800

    modify success.js

--- router/route/success.js
+++ router/route/success.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.render('success', {amount: 89});
+  res.render('success', {amount: 200});
 });
 
 module.exports = router;
