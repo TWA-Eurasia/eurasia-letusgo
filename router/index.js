32ba119d9a55e3017f1e64855ea960a75ad1596a
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-31 15:36:59 +0800

    modify some code

--- router/index.js
+++ router/index.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
     app.use('/helloWorld', require('./route/helloWorld'));
     app.use('/itemDetails', require('./route/itemDetails'));
 
+
     app.use('/api/item', require('./route/item'));
 
     app.use('/orderList', require('./route/orderList'));
