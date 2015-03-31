d545b203d7791ede1674251fd00e95f12bf87377
zhzhang <xiaoyanzhuzzh@sina.com>
2015-04-01 00:07:07 +0800

    modify some code in jade view

--- router/route/category.js
+++ router/route/category.js
@@ -1,3 +1,18 @@
-/**
- * Created by zhzhang on 3/31/15.
- */
+var express = require('express');
+var router = express.Router();
+var Category = require('../../model/category');
+var Item = require('../../model/item');
+
+
+router.get('/:id', function (req, res, next) {
+  var id = req.params.id;
+
+  Item.find({category: id}, function(err, items){
+
+    if (err) return next(err);
+    console.log(items);
+    res.send(items);
+  });
+});
+
+module.exports = router;
