94dbb55b56b325c3ab8be00e35d0ade1cdde2344
yfwang <yufei930101@gmail.com>
2015-03-31 15:11:29 +0800

    update index.js route

--- router/route/index.js
+++ router/route/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Item = require('../../model/item');
 
 var _ = require('lodash');
 
-router.get('/:pageNumber', function (req, res) {
+router.get('/:pageNumber', function(req, res) {
 
+  var pageNumber = req.params.pageNumber;
+  var pageSize = 2;
+  var start = (pageNumber - 1) * pageSize;
 
-  res.render('index');
+  Item.find().skip(start).limit(pageSize).exec(function (err, items) {
+    if (err) return next(err);
+    res.render('index', {items: items});
+  });
+});
+
+router.post('/', function(req, res) {
+  Item.create({name: 'i', unit: '瓶', price: 3.5, imageUrl: 'image/cat2.png', state: 'recommend'}, function(err, item) {
+    res.send(item);
+  });
 });
 
 module.exports = router;
