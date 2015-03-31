988505db58399975bec3a25d7cc74a1b8ea57aa3
zhzhang <xiaoyanzhuzzh@sina.com>
2015-04-01 00:05:42 +0800

    add jquery code

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -5,5 +5,16 @@ var _ = require('lodash');
 
 $(document).ready(function () {
 
+  $('.item.secondMenu').on('click', function () {
 
+    var id = this.id;
+    var parentId = this.closest('.firstMenu').id;
+    console.log(id + parentId);
+
+    $.get('/api/category/' + id, {id : id})
+      .success(function (data) {
+
+        console.log(data);
+      })
+  })
 });
