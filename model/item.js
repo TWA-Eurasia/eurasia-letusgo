3570efed7c71b14c0837ad1d2eb347a9503f5526
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-31 22:52:38 +0800

    modify item database

--- model/item.js
+++ model/item.js
@@ -3,17 +3,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ItemSchema = new Schema({
+  image: String,
   name: String,
   unit: String,
-  brand: String,
   price: Number,
-  leftNumber: Number,
-  saleNumber: {type: Number, default: 0},
-  imageUrl: String,
+  inventory: Number,
+  specification: String,
   description: String,
   state: {type: String, default: 'new'},
-  category: {type: Schema.ObjectId, ref: 'Category'},
-  specification: String
+  category: {type: Schema.ObjectId, ref: 'Category'}
 });
 
 module.exports = mongoose.model('Item',ItemSchema);
