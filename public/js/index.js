da2e7132a118c3d2ebbd8389b67f42f791e18cc2
yfwang <yufei930101@gmail.com>
2015-03-31 15:08:51 +0800

    update index.js in route

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,24 @@
-/**
- * Created by yfwang on 3/31/15.
- */
+'use strict';
+
+var _ = require('lodash');
+var $ = require('./jquery.pagination');
+
+require('semantic-ui');
+
+$(document).ready(function () {
+
+  $('.pageNumber').on('click', function() {
+    var pageNumber = parseInt(this.id);
+    console.log(pageNumber);
+
+    $.ajax({
+      url: '/index/' + pageNumber,
+      type: 'GET',
+      success: function(result) {
+        $(location).attr('href', '/index/'+ pageNumber);
+      }
+    });
+
+  })
+
+});
