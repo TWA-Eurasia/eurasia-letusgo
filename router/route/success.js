6344eb50d0c8d931128cb69688934e13099a1b16
fgcui <fgcui1204@163.com>
2015-03-31 10:58:28 +0800

    modified success.js

--- router/route/success.js
+++ router/route/success.js
@@ -1,3 +1,9 @@
-/**
- * Created by fgcui on 3/31/15.
- */
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+  res.render('success', {amount: 89});
+});
+
+module.exports = router;
