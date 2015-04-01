a961e0d150633ba49bb73ca95a65049e3b81fe9f
fgcui <fgcui1204@163.com>
2015-04-01 16:16:28 +0800

    modify render

--- router/route/success.js
+++ router/route/success.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.render('success', {amount: 200});
+  res.render('success');
 });
 
 module.exports = router;
