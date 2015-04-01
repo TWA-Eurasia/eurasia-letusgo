af749b45656c300e92c39c84266f8ea950c9b64a
dale <18717394542@163.com>
2015-04-01 11:16:51 +0800

    modify cart.js

--- router/route/cart.js
+++ router/route/cart.js
@@ -7,7 +7,7 @@ var Item = require('../../model/item.js');
 
 router.get('/', function (req, res, next) {
   var cartId = req.id;
-  cartId = "551abc6a1f47599826402314";
+  cartId = "551b622606449c424685951a";
   Cart.findById(cartId)
     .populate('cartItems.item')
     .exec(function (err, cart) {
@@ -45,7 +45,7 @@ router.post('/', function (req, res, next) {
 router.delete('/:cartItemId', function (req, res) {
   var cartItemId = req.params.cartItemId;
   //var cartId = req.body.cartId;
-  var cartId = '551abc6a1f47599826402314';
+  var cartId = '551b622606449c424685951a';
 
   Cart.findById(cartId, function (err, cart) {
     if (err) {
