fd2c42daa7a6183dca28692ec63edebf013865c0
fgcui <fgcui1204@163.com>
2015-04-02 14:19:52 +0800

    modify method of router.get

--- router/route/cart.js
+++ router/route/cart.js
@@ -8,27 +8,26 @@ var CartItem = require('../../model/cartItem.js');
 
 router.get('/', function (req, res, next) {
   var cartId = req.id;
-
-  cartId = "551cbacf4c7e65fb42a65f0f";
+  cartId = "551cc282a6b79c584b59bc0f";
+  
   Cart.findById(cartId)
-    .populate('cartItems.item')
+    .populate('cartItems')
     .exec(function (err, cart) {
-      if (err) {
-        return next(err);
-      }
-      //console.log(cart);
-      _.forEach(cart.cartItems, function (cartItem) {
-        cartItem.subtotal = cartItem.item.price * cartItem.number;
-        cart.totalAmount += cartItem.subtotal;
-      });
-      res.render('cart', {cartItems: cart.cartItems, cart: cart});
+
+      CartItem.find()
+        .populate('item')
+        .exec(function(err, cartItems) {
+
+          var total = cart.getTotal(cartItems);
+          res.render('cart', {cartItems: cartItems, total: total});
+        });
     })
 });
 
 router.post('/', function (req, res, next) {
   //Item.create(
-  //  {name: '可乐', unit: '瓶', price: 3.5, imageUrl: 'image/kele.jpg', inventory: '100'},
-  //  {name: '雪碧', unit: '瓶', price: 4.5, imageUrl: 'image/xuebi.jpg', inventory: '100'},
+  //  {name: '可乐', unit: '瓶', price: 3.5, image: 'image/kele.jpg', inventory: '100'},
+  //  {name: '雪碧', unit: '瓶', price: 4.5, image: 'image/xuebi.jpg', inventory: '100'},
   //  function (err, item1, item2) {
   //    var itemId1 = item1._id;
   //    var itemId2 = item2._id;
@@ -42,15 +41,15 @@ router.post('/', function (req, res, next) {
   //    );
   //  }
   //);
-  //
-  //Cart.create({cartItems: ['551cb86c360ca53140292d77', '551cb86c360ca53140292d78', '551cb86c360ca53140292d79', '551cb86c360ca53140292d7a']});
+
+  Cart.create({cartItems: ['551cc20e47a654d14a280e9b', '551cc20e47a654d14a280e9c', '551cc20e47a654d14a280e9d', '551cc20e47a654d14a280e9e']});
 
 });
 
 router.delete('/:cartItemId', function (req, res) {
   var cartItemId = req.params.cartItemId;
   //var cartId = req.body.cartId;
-  var cartId = '551cbacf4c7e65fb42a65f0f';
+  var cartId = '551cc282a6b79c584b59bc0f';
 
   Cart.findById(cartId, function (err, cart) {
     if (err) {
