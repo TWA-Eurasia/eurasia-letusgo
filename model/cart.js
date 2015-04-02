14d7baaeb2e068e254238add35deb0abeabd0838
fgcui <fgcui1204@163.com>
2015-04-02 11:48:30 +0800

    add getTotal method

--- model/cart.js
+++ model/cart.js
@@ -4,8 +4,18 @@ var Schema = mongoose.Schema;
 
 var CartSchema = new Schema({
   user: {type: Schema.ObjectId, ref: 'User'},
-  cartItems: [{
-    cartItem: {type: Schema.ObjectId, ref: 'CartItem'}
-  }]
+  cartItems: [{type: Schema.ObjectId, ref: 'CartItem'}
+  ]
 });
+
+//CartSchema.methods = {
+//  getTotal: function () {
+//    var totalAmount = 0;
+//    this.cartItems.forEach(function (cartItem) {
+//      totalAmount += cartItem.getSubtotal();
+//    });
+//    return totalAmount;
+//  }
+//}
+
 module.exports = mongoose.model('Cart', CartSchema);
