023bcb4232385122167eb6b4b9278166ab459b53
fgcui <fgcui1204@163.com>
2015-04-02 11:48:57 +0800

    modify create data to db

--- router/route/cart.js
+++ router/route/cart.js
@@ -4,11 +4,12 @@ var router = express.Router();
 
 var Cart = require('../../model/cart.js');
 var Item = require('../../model/item.js');
+var CartItem = require('../../model/cartItem.js');
 
 router.get('/', function (req, res, next) {
   var cartId = req.id;
 
-  cartId = "551c9f3c3f105588216814db";
+  cartId = "551cbacf4c7e65fb42a65f0f";
   Cart.findById(cartId)
     .populate('cartItems.item')
     .exec(function (err, cart) {
@@ -25,28 +26,31 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  Item.create(
-    {name: '可乐', unit: '瓶', price: 3.5, imageUrl: 'image/kele.jpg'},
-    {name: '雪碧', unit: '瓶', price: 3.5, imageUrl: 'image/xuebi.jpg'},
-    function (err, item1, item2) {
+  //Item.create(
+  //  {name: '可乐', unit: '瓶', price: 3.5, imageUrl: 'image/kele.jpg', inventory: '100'},
+  //  {name: '雪碧', unit: '瓶', price: 4.5, imageUrl: 'image/xuebi.jpg', inventory: '100'},
+  //  function (err, item1, item2) {
+  //    var itemId1 = item1._id;
+  //    var itemId2 = item2._id;
+  //
+  //    console.log(itemId1);
+  //    CartItem.create(
+  //      {item: itemId1, number: 6},
+  //      {item: itemId2, number: 2},
+  //      {item: itemId1, number: 8},
+  //      {item: itemId2, number: 6}
+  //    );
+  //  }
+  //);
+  //
+  //Cart.create({cartItems: ['551cb86c360ca53140292d77', '551cb86c360ca53140292d78', '551cb86c360ca53140292d79', '551cb86c360ca53140292d7a']});
 
-      var itemId1 = item1._id;
-      var itemId2 = item2._id;
-
-
-      var cart = new Cart();
-      cart.cartItems.push({item: itemId1, number: 6});
-      cart.cartItems.push({item: itemId2, number: 7});
-      cart.save(function (err, cart) {
-        res.send(cart);
-      })
-    });
 });
 
 router.delete('/:cartItemId', function (req, res) {
   var cartItemId = req.params.cartItemId;
   //var cartId = req.body.cartId;
-  var cartId = '551c9f3c3f105588216814db';
+  var cartId = '551cbacf4c7e65fb42a65f0f';
 
   Cart.findById(cartId, function (err, cart) {
     if (err) {
