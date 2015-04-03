4ec8d0f7d79905c94088aa1edde0c6c79babc407
fgcui <fgcui1204@163.com>
2015-04-03 09:35:30 +0800

    fix merge

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
