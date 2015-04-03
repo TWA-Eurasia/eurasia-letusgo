da0f95cd5f57b461620d2ef392373486d8c4107f
liurong <570899307@qq.com>
2015-04-03 17:35:27 -0400

    update code with new item schema

--- model/item.js
+++ model/item.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
-var _ = require('lodash');
+
 var ItemSchema = new Schema({
   name: String,
   unit: String,
@@ -18,28 +18,5 @@ var ItemSchema = new Schema({
   }],
   isRecommend: Boolean
 });
-
-ItemSchema.statics.getDetails = function(items) {
-  var details = [];
-
-  items.forEach(function(item) {
-
-    if (item.specification !== '') {
-      var detail = {
-        price: item.price,
-        specification: item.specification,
-        inventory: item.inventory
-      };
-
-      details.push(detail);
-    }
-  });
-
-  if (details.length > 0) {
-    _.first(details).isChecked = "checked";
-  }
-
-  return details;
-};
 
 module.exports = mongoose.model('Item', ItemSchema);
