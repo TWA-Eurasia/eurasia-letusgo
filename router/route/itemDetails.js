2679ce76baf9108c165422ee33c46b2ab1cb77cc
liurong <570899307@qq.com>
2015-04-03 17:35:27 -0400

    update code with new item schema

--- router/route/itemDetails.js
+++ router/route/itemDetails.js
@@ -3,34 +3,28 @@ var router = express.Router();
 
 var Item = require('../../model/item');
 var Category = require('../../model/category');
-var _ = require('lodash');
+
 router.get('/:id', function(req, res) {
   var id = req.params.id;
 
-  Item.findById(id, function(err, item) {
+  Item.findById(id)
+    .populate('category')
+    .exec(function(err, item) {
 
-    Item.find({
-        name: item.name
-      })
-      .populate('category')
-      .exec(function(err, items) {
+      Category.findById(item.category._id)
+        .populate('parent')
+        .exec(function(err, category) {
 
-        Category.findById(_.first(items).category._id)
-          .populate('parent')
-          .exec(function(err, category) {
+          var itemDetails = {
+            item: item,
+            category: category
+          };
 
-            var itemDetails = {
-              item: _.first(items),
-              details: Item.getDetails(items),
-              category: category,
-            };
-
-            res.render('itemDetails', {
-              itemDetails: itemDetails
-            });
+          res.render('itemDetails', {
+            itemDetails: itemDetails
           });
-      });
-  });
+        });
+    });
 });
 
 module.exports = router;
