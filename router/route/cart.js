bba128bebecc12fe9f3614a2be9db1d37f71ec0c
dale <18717394542@163.com>
2015-04-04 17:38:35 +0800

    rewrite Item.create and Cart.create

--- router/route/cart.js
+++ router/route/cart.js
@@ -8,7 +8,7 @@ var CartItem = require('../../model/cartItem.js');
 
 router.get('/', function (req, res) {
   var cartId = req.id;
-  cartId = "551cc282a6b79c584b59bc0f";
+  cartId = "551fb07aa070742e4df671b4";
 
   Cart.findById(cartId)
     .populate('cartItems')
@@ -42,30 +42,39 @@ router.post('/:id', function (req, res, next) {
   });
 
   //Item.create(
-  //  {name: '可乐', unit: '瓶', price: 3.5, image: 'image/kele.jpg', inventory: '100'},
-  //  {name: '雪碧', unit: '瓶', price: 4.5, image: 'image/xuebi.jpg', inventory: '100'},
-  //  function (err, item1, item2) {
+  //  {name: '可乐', unit: '瓶', price: 3.00, image: 'image/kele.jpg', inventory: '100'},
+  //  {name: '雪碧', unit: '瓶', price: 3.00, image: 'image/xuebi.jpg', inventory: '100'},
+  //  {name: 'Nestle雀巢(三合一)速溶咖啡180ml 罐装', unit: '罐', price: 4.50, image: 'image/coffee.png', inventory: '200'},
+  //  {name: 'RIO(锐澳)鸡尾酒 青柠+朗姆 275ml', unit: '瓶', price: 14.50, image: 'image/rio.jpg', inventory:'300'},
+  //  function (err, item1, item2, item3, item4) {
   //    var itemId1 = item1._id;
   //    var itemId2 = item2._id;
+  //    var itemId3 = item3._id;
+  //    var itemId4 = item4._id;
   //
   //    console.log(itemId1);
   //    CartItem.create(
   //      {item: itemId1, number: 6},
   //      {item: itemId2, number: 2},
-  //      {item: itemId1, number: 8},
-  //      {item: itemId2, number: 6}
+  //      {item: itemId4, number: 8},
+  //      {item: itemId3, number: 6},
+  //      {item: itemId1, number: 14},
+  //      {item: itemId2, number: 20},
+  //      {item: itemId4, number: 1},
+  //      {item: itemId3, number: 6}
   //    );
   //  }
   //);
 
-  //Cart.create({cartItems: ['551cc20e47a654d14a280e9b', '551cc20e47a654d14a280e9c', '551cc20e47a654d14a280e9d', '551cc20e47a654d14a280e9e']});
-
+  //Cart.create({cartItems: ['551fb01d668684584c5f3e50', '551fb01d668684584c5f3e51', '551fb01d668684584c5f3e52',
+  //                         '551fb01d668684584c5f3e53', '551fb01d668684584c5f3e54', '551fb01d668684584c5f3e55',
+  //                         '551fb01d668684584c5f3e56', '551fb01d668684584c5f3e57']});
 });
 
 router.delete('/:cartItemId', function (req, res) {
   var cartItemId = req.params.cartItemId;
   //var cartId = req.body.cartId;
-  var cartId = '551cc282a6b79c584b59bc0f';
+  var cartId = '551fb07aa070742e4df671b4';
 
   Cart.findById(cartId, function (err, cart) {
     if (err) {
