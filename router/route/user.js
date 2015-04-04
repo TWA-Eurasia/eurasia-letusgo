076fb86ec66e0d10d303f91146c4355c7183bb69
mkyang <303055758@qq.com>
2015-04-04 20:09:25 +0800

    modified user.js

--- router/route/user.js
+++ router/route/user.js
@@ -8,15 +8,7 @@ router.post('/:id', function(req, res){
   var userId = req.params.id;
   var indentId = req.body.indentId;
 
-//  User.update({_id: userId}, {$set:{indents : [indentId]}}, function(err, user){
-//console.log(user.indents);
-//    res.send('add indent to user is successful');
-//  });
-//  551cbc22ff9c98ae4e3d38fa
-console.log('user: ' + userId);
-
-  User.update(userId,  {$set: {indents: ['551cbc22ff9c98ae4e3d38fa']}}, function(err, user){
-console.log(user+ '================');
+  User.update(userId,  {$addToSet: {indents: indentId}}, function(err, user){
     res.send('add indent to user is successful');
   });
 });
