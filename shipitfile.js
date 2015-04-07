aa8b5effc95f28f916c0441e4296df8470ea21bf
yfwang <yufei930101@gmail.com>
2015-04-07 18:49:32 +0800

    add shipitfile.js

---
+++ shipitfile.js
@@ -0,0 +1,21 @@
+/**
+ * Created by yfwang on 4/7/15.
+ */
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/github-monitor',
+      deployTo: '/tmp/deploy_to',
+      repositoryUrl: 'git@github.com:TWA-Eurasia/eurasia-letusgo.git',
+      branch: 'development',
+      ignores: ['.git', 'node_modules', '.idea', 'eurasia-letusgo.iml', 'jspm_packages', '.DS_Store'],
+      keepReleases: 2,
+      shallowClone: true
+    },
+    staging: {
+      servers: 'root@120.26.45.145'
+    }
+  });
+};
