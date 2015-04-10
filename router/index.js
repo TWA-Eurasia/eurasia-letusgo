f632520623a2f3a8d852c9e38c5cbfb44d7c642c
hawang <654296217@qq.com>
2015-04-10 11:25:55 +0800

    unify the path

--- router/index.js
+++ router/index.js
@@ -1,15 +1,15 @@
 module.exports = function (app) {
 
     app.use('/', require('./route/index'));
-    app.use('/helloWorld', require('./route/helloWorld'));
+    app.use('/success', require('./route/success'));
+    app.use('/indent', require('./route/indentPage'));
+    app.use('/cart',require('./route/cart'));
     app.use('/items', require('./route/itemDetails'));
+
+    app.use('/helloWorld', require('./route/helloWorld'));
 
     app.use('/api/item', require('./route/item'));
     app.use('/api/category', require('./route/category'));
-    app.use('/cart',require('./route/cart'));
-
-    app.use('/indent', require('./route/indentPage'));
-    app.use('/success', require('./route/success'));
     app.use('/api/user', require('./route/user'));
     app.use('/api/indent', require('./route/indent'));
 
