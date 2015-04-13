93363ce2fe9c1d9c2772564d54116ed20fb79e57
hawang <654296217@qq.com>
2015-04-13 18:06:34 +0800

    add indentTest.js

---
+++ test/spec/indentTest.js
@@ -0,0 +1,47 @@
+var request = require('superTest');
+
+var app = require('../../app.js');
+
+describe('GET api/indent', function () {
+
+  it('should get total and indent', function (done) {
+
+      request(app)
+        .get('/api/indent')
+        .expect('Content-type', /json/)
+        .expect(200, function(err, res) {
+
+          expect(res.body).to.have.property('indent');
+          expect(res.body.total).to.equal('2676.00');
+
+          done();
+        });
+  });
+});
+
+//describe('GET api/indent', function () {
+//
+//  it('should get total and indent', function (done) {
+//
+//    request(app)
+//      .post('/api/indent', function() {
+//
+//        var spy, req, res;
+//         res = req = {};
+//
+//        spy = res.send = sinon.spy();
+//
+//        callback(spy);
+//        expect(spy).to.have.been.called();
+//      })
+//      .end(done);
+//      //.expect('Content-type', /json/)
+//      //.expect(200, function(err, res) {
+//      //
+//      //  expect(res.body).to.have.property('indent');
+//      //  expect(res.body.total).to.equal('2676.00');
+//      //
+//      //  done();
+//      //});
+//  });
+//});
