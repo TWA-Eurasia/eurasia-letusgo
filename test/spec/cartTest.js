97e4d201b6e0ec121dd09825ad2dcd760046db19
mkyang <303055758@qq.com>
2015-04-14 10:18:43 +0800

    add test get /

---
+++ test/spec/cartTest.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var app = require('../../app.js');
+
+var router = require('../../router/route/cart');
+
+describe('cart', function() {
+
+  describe('GET /', function() {
+
+    it('should return 200 ok', function() {
+
+      request(app)
+        .get('/')
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .end(function(err) {
+          if(err) {throw err;}
+        })
+    });
+  });
+});
