b4b3e2d0a2de81ff4c9096788c072e909ed923f6
fgcui <fgcui1204@163.com>
2015-04-14 23:52:05 +0800

    add 'use strict'

--- router/controller/getItem.js
+++ router/controller/getItem.js
@@ -1,4 +1,4 @@
-
+'use strict';
 var Item = require('../../model/item');
 var Category = require('../../model/category');
 
--- test/spec/itemsDetailsTest.js
+++ test/spec/itemsDetailsTest.js
@@ -1,3 +1,4 @@
+'use strict';
 describe('GET /', function () {
 
   var resMock = {};
