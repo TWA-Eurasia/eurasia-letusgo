e7e66b5e301a90df38eb5b81c342b2dd1ed71a75
hawang <654296217@qq.com>
2015-04-14 10:32:40 +0800

    add cartitems.js in seed

--- seed/test/cartitems.js
+++ seed/test/cartitems.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var cartItems = [
   {"__v":0,"_id":{"$oid":"551cc20e47a654d14a280e9b"},"item":{"$oid":"5523cea79294d58a8e06c3bf"},"number":6},
   {"__v":0,"_id":{"$oid":"551cc20e47a654d14a280e9c"},"item":{"$oid":"5523cea79294d58a8e06c3c0"},"number":2},
