69551c8c17c8ece85257cc055287f8a4f8936aba
hawang <654296217@qq.com>
2015-04-14 11:01:40 +0800

    delete useless codes

--- seed/test/indents.js
+++ seed/test/indents.js
@@ -3,9 +3,9 @@
 var indents = [
   {
     createDate : { $date : 2010 },
-    _id : { $oid : '551fd16975cd55ed0cfa5503' },
+    _id : '551fd16975cd55ed0cfa5503',
     isPaid : true,
-    cartItems : [ { $oid : '551cc20e47a654d14a280e9b' }, { $oid : '551cc20e47a654d14a280e9c' }, { $oid : '551cc20e47a654d14a280e9d' }, { $oid : '551cc20e47a654d14a280e9e' } ],
+    cartItems : ['551cc20e47a654d14a280e9b', '551cc20e47a654d14a280e9c', '551cc20e47a654d14a280e9d', '551cc20e47a654d14a280e9e'],
     __v : 0
   }
 ];
