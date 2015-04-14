8bf4238638f1f12c7171290b62d326a678aae6c1
hawang <654296217@qq.com>
2015-04-14 11:23:37 +0800

    define global variables

--- test/common.js
+++ test/common.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var mongoose = require('mongoose');
+
 var express = require('express');
 
 var request = require('supertest');
@@ -14,3 +14,48 @@ chai.use(sinonChai);
 
 var sinon = require('sinon');
 global.sinon = sinon;
+
+var app = require('../app.js');
+global.app = app;
+
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/letusgoTest', function (err) {
+  if (err) {
+    console.log('connection error', err);
+  } else {
+    console.log('connection successful');
+  }
+});
+
+var Item = require('../model/item');
+global.Item = Item;
+var CartItem = require('../model/cartItem');
+global.CartItem = CartItem;
+var Cart = require('../model/cart');
+global.Cart = Cart;
+var Category = require('../model/category');
+global.Category = Category;
+var Indent = require('../model/indent');
+global.Indent = Indent;
+
+
+var items = require('../seed/test/items');
+global.items = items;
+var cartItems = require('../seed/test/cartItems');
+global.cartItems = cartItems;
+var indents = require('../seed/test/indents');
+global.indents = indents;
+var categories = require('../seed/test/categories');
+global.categories = categories;
+var carts = require('../seed/test/carts');
+global.carts = carts;
+
+
+Item.create(items);
+CartItem.create(cartItems);
+Cart.create(carts);
+Category.create(categories);
+Indent.create(indents);
+
