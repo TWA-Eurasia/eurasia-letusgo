4b3fc42249bc1d8f7665abd5bb26ee0810b4c105
hawang <654296217@qq.com>
2015-04-14 10:47:18 +0800

    add carts.js and indent.js in seed/test

---
+++ seed/test/carts.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var carts = [
+  {__v:0,_id:{$oid:'551cc282a6b79c584b59bc0f'},cartItems:[{$oid:'551cc20e47a654d14a280e9b'},{$oid:'551cc20e47a654d14a280e9c'},{$oid:'551cc20e47a654d14a280e9d'},{$oid:'551cc20e47a654d14a280e9e'}]}
+];
+
+module.exports = carts;
