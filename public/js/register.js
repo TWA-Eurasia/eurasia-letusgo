297fb78e0ce4780d6b0ec36b89f6bf1bda6b103c
zhzhang <xiaoyanzhuzzh@sina.com>
2015-04-14 16:07:52 +0800

    can input chinese

--- public/js/register.js
+++ public/js/register.js
@@ -6,19 +6,85 @@ require('github/ziyiking/Semantic-UI@master/dist/semantic');
 $(function () {
 
 
-  $('#userName').on('change', function (){
+  $('#userName').on('blur', function () {
 
     $('#userNameMessage').hide();
 
     var userName = $('#userName').val();
-    var userNameReg = /^(\w){6,13}$/;
+    var userNameLength = userName.replace(/[^x00-xff]/g,"**").length;
 
-    if (!userNameReg.exec(userName)) {
+    if (userName === '') {
 
+      $('#userNameMessage').html('用户名不能为空');
+      $('#userNameMessage').show();
+
+    } else if (userNameLength < 6 || userNameLength > 20) {
+
+      $('#userNameMessage').html('用户名至少为6-20位字符');
       $('#userNameMessage').show();
     }
   });
-  
+
+  $('#password').on('blur', function (){
+
+    $('#passwordMessage').hide();
+
+    var password = $('#password').val();
+    var passwordReg = /^(\w){6,20}$/;
+
+    if (password === '') {
+
+      $('#passwordMessage').html('密码不能为空');
+      $('#passwordMessage').show();
+
+    } else if (!passwordReg.exec(password)) {
+
+      $('#passwordMessage').html('密码至少为6-20位字符');
+      $('#passwordMessage').show();
+    }
+  });
+
+  $('#repeatPassword').on('blur', function (){
+
+    $('#repeatPasswordMessage').hide();
+
+    var password = $('#password').val();
+    var repeatPassword = $('#repeatPassword').val();
+
+    if (repeatPassword === '') {
+
+      $('#repeatPasswordMessage').html('重复密码不能为空');
+      $('#repeatPasswordMessage').show();
+
+    } else if (repeatPassword !== password) {
+
+      $('#repeatPasswordMessage').html('用户两次密码输入不一致');
+      $('#repeatPasswordMessage').show();
+    }
+  });
+
+  $('#email').on('blur', function (){
+
+    $('#emailMessage').hide();
+
+    var email = $('#email').val();
+    var emailReg = /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
+
+    if (email === '') {
+
+      $('#emailMessage').html('邮箱不能为空');
+      $('#emailMessage').show();
+
+    } else if (!emailReg.exec(email)) {
+
+      $('#emailMessage').html('请填写正确邮箱的格式');
+      $('#emailMessage').show();
+    }
+  });
+
+
+
+
   $('#submitButton').on('click', function (){
 
     var user = {
@@ -45,7 +111,6 @@ $(function () {
       console.log('false');
     }
 
-    var emailReg = /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
     var userNameReg = /^(\w){6,13}$/;
     if (userNameReg.exec(userName)) {
       user.userName = userName;
