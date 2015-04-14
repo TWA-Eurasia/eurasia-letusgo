719d8143bbe207f1e6b2d7a3cd089a76f370b0fb
fgcui <fgcui1204@163.com>
2015-04-14 15:44:34 +0800

    extract getItem function from itemDetails.js to getItem.js

--- router/controller/getItem.js
+++ router/controller/getItem.js
@@ -1,3 +1,27 @@
-/**
- * Created by fgcui on 4/14/15.
- */
+var Item = require('../../model/item');
+var Category = require('../../model/category');
+
+var getItem = function(req, res) {
+  var id = req.params.id;
+
+  Item.findById(id)
+    .populate('category')
+    .exec(function (err, item) {
+
+      Category.findById(item.category._id)
+        .populate('parent')
+        .exec(function (err, category) {
+          var itemDetails = {
+            item: item,
+            category: category
+          };
+
+          res.render('itemDetails', {
+            itemDetails: itemDetails
+          });
+
+        });
+    });
+};
+
+module.exports = getItem;
