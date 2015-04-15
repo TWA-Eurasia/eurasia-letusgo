db98dc4fb8d34c779d9f9693e27c85233e2ae53a
mkyang <303055758@qq.com>
2015-04-15 09:31:36 +0800

    format controller/cart.js and route/cart.js

--- router/controller/cart.js
+++ router/controller/cart.js
@@ -30,4 +30,6 @@ var getCart = function(req, res) {
     });
 };
 
+
+
 module.exports = getCart;
--- router/route/cart.js
+++ router/route/cart.js
@@ -8,33 +8,12 @@ var Cart = require('../../model/cart.js');
 var Item = require('../../model/item.js');
 var CartItem = require('../../model/cartItem.js');
 
+var getCart = require('../controller/getCart');
+
 router.get('/', function (req, res) {
+
   getCart(req, res);
 
-
-
-  //var cartId = "551cc282a6b79c584b59bc0f";
-  //
-  //Cart.findById(cartId)
-  //  .populate('cartItems')
-  //  .exec(function (err, cart) {
-  //    Item.populate(cart, 'cartItems.item', function (err) {
-  //      if (err) {
-  //        throw err
-  //      }
-  //
-  //      _.map(cart.cartItems, function (cartItem) {
-  //        cartItem.item.shortName = cartItem.item.name;
-  //
-  //        if (cartItem.item.name.length > 8) {
-  //          cartItem.item.shortName = cartItem.item.name.substring(0, 8) + '..';
-  //        }
-  //      });
-  //
-  //      var total = cart.getTotal(cart.cartItems);
-  //      res.render('cart', {cartItems: cart.cartItems, total: total});
-  //    });
-  //  });
 });
 
 router.post('/:id', function (req, res) {
