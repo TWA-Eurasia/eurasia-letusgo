272a07c3fc61fab5ca4925971bfb5ca325758f21
zhzhang <xiaoyanzhuzzh@sina.com>
2015-04-15 23:26:37 +0800

    modify selectorName to -

--- public/js/register.js
+++ public/js/register.js
@@ -5,15 +5,15 @@ require('github/ziyiking/Semantic-UI@master/dist/semantic');
 
 $(function () {
 
-  $('#userName').on('blur', function () {
+  $('#user-name').on('blur', function () {
 
-    var $userNameMessage = $('#userNameMessage');
+    var $userNameMessage = $('#user-name-message');
     $userNameMessage.hide();
 
-    var $userNameCorrect = $('#userNameCorrect');
+    var $userNameCorrect = $('#user-name-correct');
     $userNameCorrect.hide();
 
-    var userName = $('#userName').val().trim('');
+    var userName = $('#user-name').val().trim('');
     var userNameLength = userName.replace(/[^x00-xff]/g,'**').length;
 
     if (userName === '') {
@@ -31,10 +31,10 @@ $(function () {
 
   $('#password').on('blur', function (){
 
-    var $passwordMessage = $('#passwordMessage');
+    var $passwordMessage = $('#password-message');
     $passwordMessage.hide();
 
-    var $passwordCorrect = $('#passwordCorrect');
+    var $passwordCorrect = $('#password-correct');
     $passwordCorrect.hide();
 
 
@@ -54,16 +54,16 @@ $(function () {
     }
   });
 
-  $('#repeatPassword').on('blur', function (){
+  $('#repeat-password').on('blur', function (){
 
-    var $repeatPasswordMessage = $('#repeatPasswordMessage');
+    var $repeatPasswordMessage = $('#repeat-password-message');
     $repeatPasswordMessage.hide();
 
-    var $repeatPasswordCorrect = $('#repeatPasswordCorrect');
+    var $repeatPasswordCorrect = $('#repeat-password-correct');
     $repeatPasswordCorrect.hide();
 
     var password = $('#password').val().trim('');
-    var repeatPassword = $('#repeatPassword').val().trim('');
+    var repeatPassword = $('#repeat-password').val().trim('');
 
     if (repeatPassword === '') {
 
@@ -81,10 +81,10 @@ $(function () {
 
   $('#email').on('blur', function (){
 
-    var $emailMessage = $('#emailMessage');
+    var $emailMessage = $('#email-message');
     $emailMessage.hide();
 
-    var $emailCorrect = $('#emailCorrect');
+    var $emailCorrect = $('#email-correct');
     $emailCorrect.hide();
 
     var email = $('#email').val().trim('');
@@ -104,96 +104,26 @@ $(function () {
     }
   });
 
-  $('#resetButton').on('click', function() {
+  $('#reset-button').on('click', function() {
 
-    $('#userName').val('');
+    $('#user-name').val('');
     $('#password').val('');
-    $('#repeatPassword').val('');
-    $('#phoneNumber').val('');
+    $('#repeat-password').val('');
+    $('#phone-number').val('');
     $('#address').val('');
     $('#email').val('');
   });
 
-  $('#submitButton').on('click', function() {
+  $('#submit-button').on('click', function() {
 
     var isCorrect = true;
 
-    var userName = $('#userName').val().trim('');
+    var userName = $('#user-name').val().trim('');
     var password = $('#password').val().trim('');
-    var repeatPassword = $('#repeatPassword').val().trim('');
-    var phoneNumber = $('#phoneNumber').val().trim('');
+    var repeatPassword = $('#repeat-password').val().trim('');
+    var phoneNumber = $('#phone-number').val().trim('');
     var address = $('#address').val().trim('');
     var email = $('#email').val().trim('');
-
-    //$('#userNameMessage').hide();
-    //
-    //var userNameLength = userName.replace(/[^x00-xff]/g,'**').length;
-    //
-    //if (userName === '') {
-    //
-    //  $('#userNameMessage').html('用户名不能为空').show();
-    //  isCorrect = false;
-    //
-    //} else if (userNameLength < 6 || userNameLength > 20) {
-    //
-    //  $('#userNameMessage').html('用户名至少为6-20位字符').show();
-    //  isCorrect = false;
-    //} else {
-
-  //  $('#userNameMessage').html('<i class="ui checkmark icon">').show();
-  //}
-
-    //$('#passwordMessage').hide();
-    //
-    //var passwordReg = /^(\w){6,20}$/;
-    //
-    //if (password === '') {
-    //
-    //  $('#passwordMessage').html('密码不能为空').show();
-    //  isCorrect = false;
-    //
-    //} else if (!passwordReg.exec(password)) {
-    //
-    //  $('#passwordMessage').html('密码至少为6-20位字符').show();
-    //  isCorrect = false;
-    //} else {
-
-  //  $('#passwordMessage').html('<i class="ui checkmark icon">').show();
-  //}
-
-    //$('#repeatPasswordMessage').hide();
-    //
-    //if (repeatPassword === '') {
-    //
-    //  $('#repeatPasswordMessage').html('重复密码不能为空').show();
-    //  isCorrect = false;
-
-    //} else if (repeatPassword !== password) {
-    //
-    //  $('#repeatPasswordMessage').html('用户两次密码输入不一致').show();
-    //  isCorrect = false;
-  //  //} else {
-  //
-  //  $('#repeatPasswordMessage').html('<i class="ui checkmark icon">').show();
-  //}
-    //
-    //$('#emailMessage').hide();
-    //
-    //var emailReg = /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/;
-    //
-    //if (email === '') {
-    //
-    //  $('#emailMessage').html('邮箱不能为空').show();
-    //  isCorrect = false;
-    //
-    //} else if (!emailReg.exec(email)) {
-    //
-    //  $('#emailMessage').html('请填写正确邮箱的格式').show();
-    //  isCorrect = false;
-    //} else {
-
-  //  $('#emailMessage').html('<i class="ui checkmark icon">').show();
-  //}
 
     if(isCorrect) {
       $('.ui.second.modal')
