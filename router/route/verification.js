6553edf2fc4debeb4b9f9e3927067a8169c21e79
fgcui <fgcui1204@163.com>
2015-04-16 17:52:29 +0800

    modify verifications js

--- router/route/verification.js
+++ router/route/verification.js
@@ -1,3 +1,10 @@
-/**
- * Created by fgcui on 4/16/15.
- */
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var verificationController = require('../../controller/verification');
+
+router.post('/verification/:id', verificationController.updateActive);
+
+module.exports = router;
