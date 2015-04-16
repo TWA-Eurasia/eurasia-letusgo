05eec17b283d1dbc26b89d681328db4271c0aeb6
fgcui <fgcui1204@163.com>
2015-04-16 17:38:10 +0800

    modify success href

--- util/email.js
+++ util/email.js
@@ -13,7 +13,7 @@ var transporter = nodeMailer.createTransport({
 var sendMail = function(data){
 
   var html = '<p>'+data.name+',您好：<p/> <p>我们收到您在 Letusgo 的注册申请，请点击下面的链接激活帐户：</p> '+
-              '<a href="http://127.0.0.1:3000">请点击本链接激活帐号 </a>';
+              '<a href="http://127.0.0.1:3000/verification/'+data._id+'">请点击本链接激活帐号 </a>';
 
   var mailOptions = {
     from: 'letusgo@letusgo.com',
