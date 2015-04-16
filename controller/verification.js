f07d0d6c9769d927cfeff2d185d77cb5b4d41e16
fgcui <fgcui1204@163.com>
2015-04-16 18:41:49 +0800

    modify controller verification

--- controller/verification.js
+++ controller/verification.js
@@ -1,3 +1,17 @@
-/**
- * Created by fgcui on 4/16/15.
- */
+'use strict';
+
+var User = require('../model/user');
+
+var updateActive = function (req, res) {
+  var id = req.params._id;
+
+  User.findById(id, function (err, user) {
+      User.update({_id: id}, {$set: {active: true}}, function () {
+        res.render('verification',{user:user});
+      });
+    });
+};
+
+module.exports = {
+  updateActive : updateActive
+};
