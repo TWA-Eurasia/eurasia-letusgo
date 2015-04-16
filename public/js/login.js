024c3a32b9571bf023e8cbf6eb92ac0157740960
mkyang <303055758@qq.com>
2015-04-16 17:14:59 +0800

    delete passport

--- public/js/login.js
+++ public/js/login.js
@@ -4,9 +4,18 @@ require('github/ziyiking/Semantic-UI@master/dist/semantic');
 
 $(function () {
 
-  $('#login').on('click', function () {
+  $('#login').on('click', function() {
 
     $('.userLogin')
       .modal('show');
   });
+
+  $('#userLogin').on('click', function() {
+
+    var userName = $('#user-name-login').val();
+    var password = $('#password-login').val();
+
+    $.post('/api/user/login', {username: userName, password: password});
+  });
 });
+
