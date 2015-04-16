8b1f5c3fc0a251a924f893dd8ac53bbd305a5245
fgcui <fgcui1204@163.com>
2015-04-16 15:38:53 +0800

    send email success

--- util/email.js
+++ util/email.js
@@ -1,31 +1,33 @@
 'use strict';
 
 var nodemailer = require('nodemailer');
-//var config = require('../config');
-var user = '877975601@qq.com';
-var pass = '2014@cfg';
-
-var transport = nodemailer.createTransport({
-  service: 'QQ',
+//var config = require('config');
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+  service: 'Hotmail',
   auth: {
-    user: user,
-    pass: pass
+    user: 'fgcui@outlook.com',
+    pass: '19921204cfg'
   }
 });
+var html = '<p>xxx,您好：<p/> <p>我们收到您在 Letusgo 的注册申请，请点击下面的链接激活帐户：</p> <a href="http://127.0.0.1:3000">请点击本链接激活帐号 </a>';
 
+// setup e-mail data with unicode symbols
 var mailOptions = {
-  from: '877975601@qq.com', // sender address
-  to: 'fgcui1204@163.com', // list of receivers
-  subject: '来自html5jq.com的激活邮件', // Subject line
-  text: '欢迎来到前端世界', // plaintext body
-  html: '欢迎来到前端世界 ' // html body
+  from: 'letusgo@letusgo.com', // sender address
+  to: 'fgcui@outlook.com', // list of receivers
+  subject: '[Letusgo] 账号激活邮件', // Subject line
+  text: '账号激活邮件', // plaintext body
+  html: html
 };
-//transport.sendMail({
-//  from    : 'Kris<' + user + '>',
-//  to      : '<fgcui1204@163.com>',
-//  subject : 'Node.JS通过SMTP协议从QQ邮箱发送邮件',
-//  html    : '这是一封测试邮件 <br> '
-//}, function(err, res) {
-//  console.log(err, res);
-//});
+
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+  if(error){
+    console.log(error);
+  }else{
+    console.log('Message sent: ' + info.response);
+  }
+});
 
