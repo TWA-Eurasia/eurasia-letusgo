0a726731ab0bf35d82a468bd21a049f749cc3ce6
mkyang <303055758@qq.com>
2015-04-16 18:05:52 +0800

    complete login modal

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,21 @@ $(function () {
     var userName = $('#user-name-login').val();
     var password = $('#password-login').val();
 
-    $.post('/api/user/login', {username: userName, password: password});
+    $.post('/api/user/login', {username: userName, password: password}, function(data) {
+
+      if(data.user) {
+        $('#loginResult').html(data.message);
+        $('.LoginResult').modal('show');
+      } else {
+        $('#loginResult').html(data.message);
+        $('.LoginResult').modal('show');
+      }
+    });
+  });
+
+  $('#confirm').on('click', function() {
+    $('.LoginResult').hide();
+    $('.userLogin').modal('show');
   });
 });
 
--- router/route/user.js
+++ router/route/user.js
@@ -4,14 +4,28 @@ var express = require('express');
 var router = express.Router();
 
 var User = require('../../model/user');
+//
+//router.post('/:id', function (req, res) {
+//
+//  var userId = req.params.id;
+//  var indentId = req.body.indentId;
+//
+//  User.update(userId, {$addToSet: {indents: indentId}}, function (err, user) {
+//    res.send('add indent to user is successful');
+//  });
+//});
 
-router.post('/:id', function (req, res) {
+router.post('/login', function(req, res) {
+  var message = '登陆成功！';
+  var username = req.body.username;
+  var password = req.body.password;
 
-  var userId = req.params.id;
-  var indentId = req.body.indentId;
+  User.findOne({'name': username}, function(err, user) {
+    if(!user || user.password !== password) {
+      message = '用户或密码错误！';
+    }
 
-  User.update(userId, {$addToSet: {indents: indentId}}, function (err, user) {
-    res.send('add indent to user is successful');
+    res.send({user: user, message: message});
   });
 });
 
