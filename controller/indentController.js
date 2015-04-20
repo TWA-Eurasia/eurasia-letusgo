62de59cd79a0eb872095303a0b707166f1e52124
zhzhang <xiaoyanzhuzzh@sina.com>
2015-04-20 10:20:12 +0800

    merge from developemt

--- controller/indentController.js
+++ controller/indentController.js
@@ -22,7 +22,6 @@ var getIndent = function (req, res) {
   //  });
 
   Indent.findById('551fd16975cd55ed0cfa5503', function (err, indent) {
-    console.log(indent);
     CartItem.populate(indent, 'cartItems', function (err, result) {
       res.send(result);
     });
