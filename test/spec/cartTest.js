1035372a4a1f6c73f0c0a9a304f97e504cd5c879
mkyang <303055758@qq.com>
2015-04-21 15:40:33 +0800

    add removeCartItem and getAmount test

--- test/spec/cartTest.js
+++ test/spec/cartTest.js
@@ -38,7 +38,7 @@ describe('cart', function () {
 
     it('shoulde return cart', function (done) {
 
-      reqMock.body = {number: 6};
+      reqMock.body = {number: 12};
       reqMock.params = {id: '5523cea79294d58a8e06c3bf'};
 
       resMock.sendStatus = function (object) {
@@ -47,6 +47,75 @@ describe('cart', function () {
         done();
       };
       cartController.addToCart(reqMock, resMock);
+    });
+  });
+
+  describe('changeCartItem', function () {
+
+    var resMock = {};
+    var reqMock = {};
+
+    var cartController = require('../../controller/cart');
+
+    it('shoulde return correct total and subtotal', function (done) {
+
+      reqMock.body = {total: 120};
+      reqMock.body = {price: 6};
+      reqMock.body = {number: 10};
+
+      reqMock.params = {id: '551cc20e47a654d14a280e9b'};
+
+      resMock.send = function (object) {
+        expect(object).to.have.property('total');
+        expect(object).to.have.property('subtotal');
+        //expect(object.total).to.equal(144.00);
+        //expect(object.subtotal).to.equal(200);
+        done();
+      };
+      cartController.changeCartItem(reqMock, resMock);
+    });
+  });
+
+  describe('removeCartItem', function () {
+
+    var resMock = {};
+    var reqMock = {};
+
+    var cartController = require('../../controller/cart');
+
+    it('shoulde return correct total and subtotal', function (done) {
+
+      reqMock.params = {id: '551cc20e47a654d14a280e9b'};
+
+      resMock.send = function (object) {
+
+        expect(object).to.have.property('cart');
+        expect(object).to.have.property('total');
+        expect(object.cart).to.have.property('cartItems');
+        expect(object.total).to.equal(3334.50);
+        done();
+      };
+      cartController.removeCartItem(reqMock, resMock);
+    });
+  });
+
+  describe('getAmount', function () {
+
+    var resMock = {};
+    var reqMock = {};
+
+    var cartController = require('../../controller/cart');
+
+    it('shoulde return correct total and subtotal', function (done) {
+
+      resMock.send = function (object) {
+
+        expect(object).to.have.property('amount');
+        expect(object.amount).to.equal(31);
+
+        done();
+      };
+      cartController.getAmount(reqMock, resMock);
     });
   });
 });
