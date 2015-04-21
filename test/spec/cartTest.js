476258822544ffafe38ae639925279a1c9b3aa92
mkyang <303055758@qq.com>
2015-04-21 16:41:43 +0800

    fix cartTest bug

--- test/spec/cartTest.js
+++ test/spec/cartTest.js
@@ -20,7 +20,7 @@ describe('cart', function () {
         expect(view).to.equal('cart');
         expect(object).to.have.property('cartItems');
         expect(object).to.have.property('total');
-        expect(object.total).to.equal(3334.50);
+        expect(object.total).to.equal('3334.50');
 
         done();
       };
@@ -36,7 +36,7 @@ describe('cart', function () {
 
     var cartController = require('../../controller/cart');
 
-    it('shoulde return cart', function (done) {
+    it('shoulde return 200 ok', function (done) {
 
       reqMock.body = {number: 12};
       reqMock.params = {id: '5523cea79294d58a8e06c3bf'};
@@ -62,6 +62,9 @@ describe('cart', function () {
       reqMock.body = {number: 12};
       reqMock.params = {id: '551cc20e47a654d14a280e9b'};
 
+      resMock.sendStatus = function (object) {
+        done();
+      };
       cartController.addToCart(reqMock, resMock);
     });
   });
