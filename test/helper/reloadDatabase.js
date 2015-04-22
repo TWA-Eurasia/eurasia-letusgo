aa41672e4633c54a8d975579635f3f7c2558c96d
liurong <570899307@qq.com>
2015-04-22 11:02:04 -0400

    add userTest and user send

--- test/helper/reloadDatabase.js
+++ test/helper/reloadDatabase.js
@@ -5,12 +5,14 @@ var CartItem = require('../../model/cartItem');
 var Cart = require('../../model/cart');
 var Category = require('../../model/category');
 var Indent = require('../../model/indent');
+var User = require('../../model/user');
 
 var items = require('../../seed/test/items');
 var cartItems = require('../../seed/test/cartItems');
 var indents = require('../../seed/test/indents');
 var categories = require('../../seed/test/categories');
 var carts = require('../../seed/test/carts');
+var users = require('../../seed/test/users');
 
 var reloadDatabase = function () {
   Item.remove({});
@@ -22,12 +24,14 @@ var reloadDatabase = function () {
   Cart.remove({});
   Category.remove({});
   Indent.remove({});
+  User.remove({});
 
   Item.create(items);
   CartItem.create(cartItems);
   Cart.create(carts);
   Category.create(categories);
   Indent.create(indents);
+  User.create(users);
 };
 
 module.exports = reloadDatabase;
