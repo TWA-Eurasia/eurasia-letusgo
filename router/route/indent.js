3dacc71f377f258f7b908e298aad15e4ed846bcb
fgcui <fgcui1204@163.com>
2015-04-23 14:25:24 +0800

    remove getIndent route

--- router/route/indent.js
+++ router/route/indent.js
@@ -5,7 +5,6 @@ var router = express.Router();
 
 var indentController = require('../../controller/indent');
 
-router.get('/', indentController.getIndent);
 router.post('/', indentController.createIndent);
 
 module.exports = router;
