41902ad5a220122b081386511d7e9c583ff7dffa
liurong <570899307@qq.com>
2015-04-23 11:57:11 -0400

    modify verificationTest

--- test/spec/verificationText.js
+++ test/spec/verificationText.js
@@ -1,31 +1,57 @@
 'use strict';
-describe('GET /', function () {
+describe('verification', function () {
 
-  var resMock = {};
-  var reqMock = {};
-
-  var verification = require('../../controller/verification');
-
-  it('it should return userName', function (done) {
-    reqMock.params = {
-      _id: '531888e2719cd8056307fd6'
-    };
-
-    resMock.render = function (view, object) {
-      expect(view).is.to.equal('verification');
-      //expect(object).to.have.property('userName');
-      //expect(object.itemDetails.item.name).to.equal('水溶C100功能饮料');
-
-      done();
-    };
-
-    verification.updateActive(reqMock, resMock);
-
-    afterEach(function () {
-
-      reloadDatabase();
-    });
-
+  afterEach(function () {
+    reloadDatabase();
   });
+
+  describe('updateActive', function () {
+
+    var resMock = {};
+    var reqMock = {};
+
+    var verification = require('../../controller/verification');
+
+    it('it should return userName', function (done) {
+      //var next = function(err) {
+      //  return err;
+      //};
+
+      reqMock.params = {
+        _id: '5523cea79294d58a8e06c3c9'
+      };
+
+
+      resMock.render = function (view, object) {
+        expect(view).is.to.property('verification');
+        //expect(object).to.have.property('userName');
+        //expect(object.userName).to.equal('Jacob KANG');
+
+        done();
+      };
+      verification.updateActive(reqMock, resMock);
+    });
+  });
+
+  describe('getUser', function () {
+    it('it should return 200 ok', function (done) {
+      var resMock = {};
+      var reqMock = {};
+      var verification = require('../../controller/verification');
+
+      reqMock.params = {
+        userName: 'Jacob KANG'
+      };
+
+      resMock.send = function (object) {
+        expect(object.state).to.equal(200);
+        //expect(object.user.name).to.have.property('user');
+        done();
+
+      };
+      verification.getUser(reqMock,resMock);
+    });
+  });
+
 });
 
