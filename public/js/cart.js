bbf09a65ac39efbe2041eaf3dedce042e223e0d2
fgcui <fgcui1204@163.com>
2015-04-23 14:10:19 +0800

    modify send data of user

--- public/js/cart.js
+++ public/js/cart.js
@@ -224,7 +224,7 @@ $(function () {
 
       $.post('/api/indent',
         {
-          user: sessionUser,
+          user: '',
           cartItems: cartItemIds,
           createDate: createDate,
           isPaid: false
@@ -237,34 +237,34 @@ $(function () {
     }
   });
 
-  $('#user-login').on('click', function () {
-
-    var userName = $('#user-name-login').val();
-    var password = $('#password-login').val();
-
-    $.post('/api/user/login', {username: userName, password: password}, function (data) {
-
-      if (data.user) {
-        sessionStorage.setItem('user', data.user._id);
-
-        var currentUserId = sessionStorage.getItem('user');
-
-        $.get('/api/user/' + currentUserId)
-          .success(function (data) {
-
-            if (!data.data.name) {
-              $('#current-user').html(data.data.name).show();
-            }
-
-            $('.user-login-modal').modal('hide');
-            $('#login-success').html(data.message);
-            $('#tips').show().fadeOut(2000);
-          });
-
-      } else {
-        $('#login-result').html(data.message).show();
-      }
-
-    });
-  });
+  //$('#user-login').on('click', function () {
+  //
+  //  var userName = $('#user-name-login').val();
+  //  var password = $('#password-login').val();
+  //
+  //  $.post('/api/user/login', {username: userName, password: password}, function (data) {
+  //
+  //    if (data.user) {
+  //      sessionStorage.setItem('user', data.user._id);
+  //
+  //      var currentUserId = sessionStorage.getItem('user');
+  //
+  //      $.get('/api/user/' + currentUserId)
+  //        .success(function (data) {
+  //
+  //          if (!data.data.name) {
+  //            $('#current-user').html(data.data.name).show();
+  //          }
+  //
+  //          $('.user-login-modal').modal('hide');
+  //          $('#login-success').html(data.message);
+  //          $('#tips').show().fadeOut(2000);
+  //        });
+  //
+  //    } else {
+  //      $('#login-result').html(data.message).show();
+  //    }
+  //
+  //  });
+  //});
 });
