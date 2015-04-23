62b920561d1776a88a5cb1865b846d5e7a538bee
mkyang <303055758@qq.com>
2015-04-23 15:45:20 +0800

    add getUserById test

--- test/spec/userTest.js
+++ test/spec/userTest.js
@@ -19,7 +19,7 @@ describe('user', function() {
 
   describe('findUser', function() {
 
-    it('should find correct user and return right message!', function() {
+    it('should find correct user and return right message!', function(done) {
 
       reqMock.query = {name: 'Jacob KANG'};
 
@@ -27,8 +27,49 @@ describe('user', function() {
 
         expect(object.state).to.equal(200);
         expect(object.data).to.equal(true);
-        expect(object.message).to.equal('成功找到用户');
+        expect(object.message).to.equal('当前用户名已被注册');
+
+        done();
       };
+
+      userController.findUser(reqMock, resMock);
+    });
+  });
+
+  describe('findUser', function() {
+
+    it('should cant find user and return useful message!', function(done) {
+
+      reqMock.query = {name: 'Jacob'};
+
+      resMock.send = function(object) {
+
+        expect(object.state).to.equal(200);
+        expect(object.data).to.equal(false);
+        expect(object.message).to.equal('用户名可用');
+
+        done();
+      };
+
+      userController.findUser(reqMock, resMock);
+    });
+  });
+
+  describe('getUserById', function() {
+
+    it('should get user and return right message!', function(done) {
+
+      reqMock.params = {id: '5523cea79294d58a8e06c3c9'};
+
+      resMock.send = function(object) {
+
+        expect(object.state).to.equal(200);
+        expect(object.message).to.equal('成功找到用户');
+
+        done();
+      };
+
+      userController.getUserById(reqMock, resMock);
     });
   });
 });
