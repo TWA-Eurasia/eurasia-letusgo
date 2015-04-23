a0418d7772563d53d617860d4646e2ee81967e04
liurong <570899307@qq.com>
2015-04-23 17:23:28 -0400

    add indentTest and indexTest code

--- test/spec/indentTest.js
+++ test/spec/indentTest.js
@@ -1,1 +1,27 @@
+'use strict';
 
+xdescribe('GET api/indent', function () {
+
+  var resMock = {};
+  var reqMock = {};
+
+  var indentController = require('../../controller/indent');
+
+  it('should get total and indent', function (done) {
+
+    resMock.send = function (object) {
+
+      expect(object).to.have.property('cartItems');
+      expect(object).to.have.property('isPaid');
+
+      done();
+    };
+
+    indent.getIndent(reqMock, resMock);
+  });
+
+  afterEach(function () {
+
+    reloadDatabase();
+  });
+});
