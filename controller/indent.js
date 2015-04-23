12daf03375d70fa0485415fdc6a12461b7a0a77a
fgcui <fgcui1204@163.com>
2015-04-23 14:24:15 +0800

    remove getIndent

--- controller/indent.js
+++ controller/indent.js
@@ -4,30 +4,6 @@ var Indent = require('../model/indent');
 var Item = require('../model/item');
 var CartItem = require('../model/cartItem');
 
-var getIndent = function (req, res) {
-
-  //Indent.findById('551fd16975cd55ed0cfa5503')
-  //  .populate('cartItems')
-  //  .exec(function (err, indent) {
-  //  console.log(indent);
-  //    Item.populate(indent, 'cartItems.item', function (err) {
-  //
-  //      if (err) {
-  //        throw err;
-  //      }
-  //
-  //      var total = indent.getTotal(indent.cartItems);
-  //      res.send({indent: indent, total: total});
-  //    });
-  //  });
-
-  Indent.findById('551fd16975cd55ed0cfa5503', function (err, indent) {
-    CartItem.populate(indent, 'cartItems', function (err, result) {
-      res.send(result);
-    });
-  });
-};
-
 var createIndent = function(req, res) {
 
   var currentIndent = req.body;
@@ -55,11 +31,11 @@ var createIndent = function(req, res) {
         };
       }
 
+    req.session.currentIndent = indent._id;
     res.send(data);
   });
 };
 
 module.exports = {
-  getIndent: getIndent,
   createIndent: createIndent
 };
