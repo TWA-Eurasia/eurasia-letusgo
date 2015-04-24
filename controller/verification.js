1baf6df7f76175ace3bcc50a996cfdadb89e20ab
fgcui <fgcui1204@163.com>
2015-04-24 18:56:30 +0800

    add create cart when active is true

--- controller/verification.js
+++ controller/verification.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var User = require('../model/user');
+var Cart = require('../model/cart');
 
 var updateActive = function (req, res, next) {
   var id = req.params.id;
@@ -10,6 +11,9 @@ var updateActive = function (req, res, next) {
     .then(function(user) {
 
       req.session.currentUserName = user.name;
+      req.session.currentUserId = user._id;
+
+      Cart.create({user: user._id});
 
       var currentUserName = req.session.currentUserName;
       res.render('verification', {currentUserName: currentUserName, userName : currentUserName});
