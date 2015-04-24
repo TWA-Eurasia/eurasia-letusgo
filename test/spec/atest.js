b2b990beb9de3be776c80989ecfc3f6ed320b64c
mkyang <303055758@qq.com>
2015-04-24 17:04:57 +0800

    create emptyTest to afterEach dbs

---
+++ test/spec/atest.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('start', function() {
+
+  afterEach(function (done) {
+    reloadDatabase(done);
+  });
+
+  it('test will be start', function() {
+
+    console.log('start!');
+  });
+});
