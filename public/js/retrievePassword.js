2efc04fe01ffcb73cf52200ed0b43422b8fb271c
mkyang <303055758@qq.com>
2015-04-27 11:06:09 +0800

    remove retrievePassword

---
+++ public/js/retrievePassword.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var $ = require('jquery');
+
+$(function () {
+
+  $('retrieve-button').on('click', function() {
+
+    var userName = $('retrieve-user-name').val();
+    var email = $('retrieve-email').val();
+
+    $
+  });
+
+});
