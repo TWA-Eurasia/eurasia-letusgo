e71a09f67d8718c9b6a3edc2f08f90cf6c7b26b9
liurong <570899307@qq.com>
2015-04-27 09:54:05 +0800

    fix the conflict

--- router/route/indent.js
+++ router/route/indent.js
@@ -6,6 +6,5 @@ var router = express.Router();
 var indentController = require('../../controller/indent');
 
 router.post('/', indentController.createIndent);
-router.get('/', indentController.getIndent);
 
 module.exports = router;
