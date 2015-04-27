5bdfdfd0ddbdec03c157061f87dfcde8c47ba951
zhzhang <xiaoyanzhuzzh@sina.com>
2015-04-27 10:04:36 +0800

    delete useless code

--- router/route/success.js
+++ router/route/success.js
@@ -4,9 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 var indentController = require('../../controller/indent');
-//router.get('/', function (req, res) {
-//  res.render('success', {currentUserName: req.session.currentUserName});
-//});
+
 router.get('/', indentController.getSuccessInfo);
 
 module.exports = router;
