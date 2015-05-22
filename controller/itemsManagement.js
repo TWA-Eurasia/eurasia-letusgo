a5f56a5283f0c51f057989f53a50afa504c5f532
hawang <654296217@qq.com>
2015-05-22 16:50:16 +0800

    use short name in item management page

--- controller/itemsManagement.js
+++ controller/itemsManagement.js
@@ -2,15 +2,23 @@
 
 var Item = require('../model/item');
 
+var FormatUtil = require('../util/formatUtil');
+var NAME_LENGTH = 16;
+
 var getItemsManagementPage = function (req, res) {
 
   Item.find()
     .then(function(items){
-      //
+
+      items.forEach(function(item){
+
+        item.shortName = FormatUtil.parseString(item.name, NAME_LENGTH);
+      });
+
       res.render('itemsManagement', {
         items: items
       });
-    })
+    });
 };
 
 module.exports = {
