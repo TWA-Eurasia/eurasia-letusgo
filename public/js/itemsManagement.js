8671df5bd4177360ab947b911347998a67cfaac3
hawang <654296217@qq.com>
2015-05-24 01:11:16 +0800

    add ready function

--- public/js/itemsManagement.js
+++ public/js/itemsManagement.js
@@ -5,39 +5,43 @@ require('github/ziyiking/Semantic-UI@master/dist/semantic');
 
 var deleteCartItem;
 
-$('.itemName').popup({
-  content: $(this).prop('data-content')
-});
+$(function(){
 
-$('.delete_item').on('click', function () {
-
-  deleteCartItem = this;
-
-  $('.delete-modal')
-    .modal('show');
-});
-
-$('.yes').on('click', function () {
-
-  var deleteId = deleteCartItem.closest('td').id;
-console.log(deleteId);
-  $.ajax({
-    url: '/itemsManagement/' + deleteId,
-    type: 'DELETE',
-
-    success: function (data) {
-      var i_data = data;
-      console.log(i_data);
-
-      $('.delete-modal').modal('hide');
-      $('.delete-message').show();
-
-      $(deleteCartItem.closest('tr').remove());
-
-      window.setTimeout(function () {
-
-        $('.delete-message').hide();
-      }, 1000);
-    }
+  $('.itemName').popup({
+    content: $(this).prop('data-content')
   });
+
+  $('.delete_item').on('click', function () {
+
+    deleteCartItem = this;
+
+    $('.delete-modal')
+      .modal('show');
+  });
+
+  $('.yes').on('click', function () {
+
+    var deleteId = deleteCartItem.closest('td').id;
+    console.log(deleteId);
+    $.ajax({
+      url: '/itemsManagement/' + deleteId,
+      type: 'DELETE',
+
+      success: function (data) {
+        var i_data = data;
+        console.log(i_data);
+
+        $('.delete-modal').modal('hide');
+        $('.delete-message').show();
+
+        $(deleteCartItem.closest('tr').remove());
+
+        window.setTimeout(function () {
+
+          $('.delete-message').hide();
+        }, 1000);
+      }
+    });
+  });
+
 });
