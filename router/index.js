d9d9559b5f92764a0b14188eed9e9e2bfbfc5764
hawang <654296217@qq.com>
2015-05-24 22:29:39 +0800

    modify the url of item management page

--- router/index.js
+++ router/index.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
   app.use('/verification',require('./route/verification'));
   app.use('/retrievePassword',require('./route/retrievePassword'));
 
-  app.use('/itemsManagement', require('./route/itemsManagement'));
+  app.use('/admin/itemsManagement', require('./route/itemsManagement'));
 
   app.use('/api/item', require('./route/item'));
   app.use('/api/user', require('./route/user'));
