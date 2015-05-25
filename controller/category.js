a63035ad1bea7ac81c77b89db4c2e33cc4bb30df
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-25 22:39:13 +0800

    create category conroller

--- controller/category.js
+++ controller/category.js
@@ -1,3 +1,20 @@
-/**
- * Created by zhzhang on 5/25/15.
- */
+'use strict';
+
+var Category = require('../model/category');
+
+var createNewCategory = function(req, res, next){
+
+  var name = req.body.name;
+  var parent = req.body.parent;
+  Category.create({name: name, parent: parent})
+    .then(function(category) {
+      res.send({state:200, data: category, message: '添加新分类'});
+    })
+    .onReject(function(err) {
+      next(err);
+    });
+};
+module.exports = {
+
+  createNewCategory: createNewCategory
+};
