390bcc116ba53d2d679f3133d784ef9f8f9c1968
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-25 14:15:14 +0800

    categoriesManagement jade codes

--- controller/categoriesManagement.js
+++ controller/categoriesManagement.js
@@ -1,13 +1,14 @@
 'use strict';
 
+var _ = require('lodash');
 var Category = require('../model/category');
-
 var FormatUtil = require('../util/formatUtil');
 var NAME_LENGTH = 16;
 
 var getCategoriesManagementInfo = function (req, res) {
 
   Category.find({})
+    .populate('parent')
     .exec()
     .then(function(categories){
 
@@ -16,8 +17,13 @@ var getCategoriesManagementInfo = function (req, res) {
         categories.shortName = FormatUtil.parseString(categories.name, NAME_LENGTH);
       });
 
+      var mainCategories = _.filter(categories, function (category) {
+
+        return category.parent === null;
+      });
+
       res.render('categoriesManagement', {
-        mainCategories: categories
+        mainCategories: mainCategories
       });
     });
 };
