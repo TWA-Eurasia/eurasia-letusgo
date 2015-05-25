4932e0c878dff0a96d87db9738d88d2f9fe68fd1
zhzhang <xiaoyanzhuzzh@sina.com>
2015-05-25 12:10:45 +0800

    add jade code of categoriesManagement

--- controller/categoriesManagement.js
+++ controller/categoriesManagement.js
@@ -1,3 +1,28 @@
-/**
- * Created by zhzhang on 5/25/15.
- */
+'use strict';
+
+var Category = require('../model/category');
+
+var FormatUtil = require('../util/formatUtil');
+var NAME_LENGTH = 16;
+
+var getCategoriesManagementInfo = function (req, res) {
+
+  Category.find({})
+    .exec()
+    .then(function(categories){
+
+      categories.forEach(function(categories){
+
+        categories.shortName = FormatUtil.parseString(categories.name, NAME_LENGTH);
+      });
+
+      res.render('categoriesManagement', {
+        mainCategories: categories
+      });
+    });
+};
+
+module.exports = {
+
+  getCategoriesManagementInfo: getCategoriesManagementInfo
+};
